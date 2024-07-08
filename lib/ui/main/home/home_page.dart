85b7fa73e5b24ffa680034c2bd8cf17c0711c5e2
AI_Nomads <chugue85@gmail.com>
2024-07-08 19:30:47 +0900

    FIX: _ìcomponents directory fix

--- lib/ui/main/home/home_page.dart
+++ lib/ui/main/home/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'components/best_seller_section.dart';
-import 'components/book_of_the_day_section.dart';
-import 'components/top_pick_section.dart';
-import 'components/history_section.dart';
+import 'package:untitled/ui/main/home/_components/best_seller_section.dart';
+import 'package:untitled/ui/main/home/_components/book_of_the_day_section.dart';
+import 'package:untitled/ui/main/home/_components/history_section.dart';
+import 'package:untitled/ui/main/home/_components/top_pick_section.dart';
 
 class HomePage extends StatelessWidget {
   @override

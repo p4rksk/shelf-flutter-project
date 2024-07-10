8dfdb02f0e1395db15bceb22832317afefd96706
kjh5848 <kjh5848@gmail.com>
2024-07-10 17:17:38 +0900

    [FEAT] 뷰어기능추가

--- lib/ui/main/home/widgets/bottom_action_bar.dart
+++ lib/ui/main/home/widgets/bottom_action_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/_core/constants/constants.dart';
+import 'package:cosmos_epub/cosmos_epub.dart';
 
 class BottomActionBar extends StatelessWidget {
   @override
@@ -40,8 +41,18 @@ class BottomActionBar extends StatelessWidget {
           Flexible(
             flex: 2,
             child: ElevatedButton(
-              onPressed: () {
-                // Add action for reading
+              onPressed: () async {
+                // 바로 읽기 버튼 클릭 시 ePub 리더 실행
+                await CosmosEpub.openAssetBook(
+                    assetPath: 'assets/epub/test01.epub',
+                    context: context,
+                    bookId: '3',
+                    onPageFlip: (int currentPage, int totalPages) {
+                      print(currentPage);
+                    },
+                    onLastPage: (int lastPageIndex) {
+                      print('We arrived to the last widget');
+                    });
               },
               style: ElevatedButton.styleFrom(
                 foregroundColor: Colors.white,

7bbb3dfc8a9595a85edf91b37eeea859b2ec986e
AI_Nomads <chugue85@gmail.com>
2024-07-10 20:09:21 +0900

    SYNC: conflict solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+import 'package:cosmos_epub/Model/book_progress_model.dart';
 import 'package:cosmos_epub/cosmos_epub.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:untitled/ui/main/home/pages/book_detail_page.dart';
-
-import 'ui/main/search/pages/category_result_page/category_result_page.dart';
-import 'ui/main/search/pages/writer_result_page/writer_result_page.dart';
+import 'package:untitled/ui/main/myshelf/my_shelf.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();

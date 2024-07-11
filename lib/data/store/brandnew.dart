76adea7310189a5359a5b6a36e33ca8c6205fc4b
hillview0303 <ujin0105@gmail.com>
2024-07-12 00:08:07 +0900

    베스트셀러 상세페이지

--- lib/data/store/brandnew.dart
+++ lib/data/store/brandnew.dart
@@ -1,58 +1,48 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:untitled/data/model/home/book_detail_data.dart';
 
-// 상태 관리 클래스
-class BookState extends StateNotifier<DateTime> {
-  BookState() : super(DateTime(2024, 10));
+final selectedMonthProvider = StateProvider<DateTime>((ref) {
+  return DateTime(2024, 10);
+});
 
-  void nextMonth() {
-    state = DateTime(state.year, state.month + 1);
-  }
+final booksProvider = Provider<List<Book>>((ref) {
+  final selectedMonth = ref.watch(selectedMonthProvider);
+  return newBooks.where((book) {
+    DateTime date = DateTime.parse(book.registrationDate);
+    return date.year == selectedMonth.year && date.month == selectedMonth.month;
+  }).toList();
+});
 
-  void previousMonth() {
-    state = DateTime(state.year, state.month - 1);
-  }
+final groupedBooksProvider = Provider<Map<String, List<Book>>>((ref) {
+  final books = ref.watch(booksProvider);
+  final selectedMonth = ref.watch(selectedMonthProvider);
 
-  List<Book> getBooksForSelectedMonth(List<Book> books) {
-    return books.where((book) {
-      DateTime date = DateTime.parse(book.registrationDate);
-      return date.year == state.year && date.month == state.month;
-    }).toList();
-  }
-
-  Map<String, List<Book>> groupBooksByWeek(List<Book> books) {
-    Map<String, List<Book>> groupedBooks = {};
-    for (var book in books) {
-      DateTime date = DateTime.parse(book.registrationDate);
-      int weekNumber = ((date.day - 1) / 7).floor() + 1;
-      String weekKey = '${state.month}월 ${_weekNumberToKorean(weekNumber)}';
-      if (groupedBooks[weekKey] == null) {
-        groupedBooks[weekKey] = [];
-      }
-      groupedBooks[weekKey]!.add(book);
+  Map<String, List<Book>> groupedBooks = {};
+  for (var book in books) {
+    DateTime date = DateTime.parse(book.registrationDate);
+    int weekNumber = ((date.day - 1) / 7).floor() + 1;
+    String weekKey = '${selectedMonth.month}월 ${_weekNumberToKorean(weekNumber)}';
+    if (groupedBooks[weekKey] == null) {
+      groupedBooks[weekKey] = [];
     }
-    return groupedBooks;
+    groupedBooks[weekKey]!.add(book);
   }
+  return groupedBooks;
+});
 
-  String _weekNumberToKorean(int weekNumber) {
-    switch (weekNumber) {
-      case 1:
-        return '첫째주';
-      case 2:
-        return '둘째주';
-      case 3:
-        return '셋째주';
-      case 4:
-        return '넷째주';
-      case 5:
-        return '다섯째주';
-      default:
-        return '${weekNumber}주';
-    }
+String _weekNumberToKorean(int weekNumber) {
+  switch (weekNumber) {
+    case 1:
+      return '첫째주';
+    case 2:
+      return '둘째주';
+    case 3:
+      return '셋째주';
+    case 4:
+      return '넷째주';
+    case 5:
+      return '다섯째주';
+    default:
+      return '${weekNumber}주';
   }
 }
-
-final bookProvider = StateNotifierProvider<BookState, DateTime>((ref) {
-  return BookState();
-});
--- lib/ui/main/home/pages/best_seller_detail_page.dart
+++ lib/ui/main/home/pages/best_seller_detail_page.dart
@@ -1,14 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:untitled/_core/constants/constants.dart';
+import 'package:untitled/_core/constants/size.dart';
+import '../../../../data/model/home/home_page_data.dart';
+import '../../../../data/store/bestseller_provider.dart';
 
-class BestSellerDetailPage extends StatelessWidget {
+class BestSellerDetailPage extends ConsumerWidget {
+  final List<String> categories = ['종합', '소설', '경제/경영', '자기계발', '시/에세이', '인문/교양'];
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedCategory = ref.watch(categoryProvider);
+    final filteredBooks = ref.watch(filteredBooksProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('전체 베스트셀러'),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(60.0),
+          child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: categories.map((category) {
+                  final isSelected = selectedCategory == category;
+                  return GestureDetector(
+                    onTap: () {
+                      ref.read(categoryProvider.notifier).state = category;
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
+                      margin: EdgeInsets.symmetric(horizontal: 6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.grey),
+                        color: isSelected ? kAccentColor3 : Colors.transparent,
+                      ),
+                      child: Text(
+                        category,
+                        style: TextStyle(
+                          color: isSelected ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('전체 베스트셀러 페이지입니다.'),
+      body: ListView.builder(
+        itemCount: filteredBooks.length,
+        itemBuilder: (context, index) {
+          RankBook book = filteredBooks[index];
+          final categoryRank = index + 1;
+
+          return Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                Image.asset(book.imagePath, width: 120, height: 120),
+                Container(
+                  width: 100,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '$categoryRank',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 10.0,
+                              color: Colors.black38,
+                              offset: Offset(2.0, 2.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        book.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(book.author),
+                      if (selectedCategory != '종합')
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: gap_s),
+                            Text(
+                              '종합 순위 ${book.rank}',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }

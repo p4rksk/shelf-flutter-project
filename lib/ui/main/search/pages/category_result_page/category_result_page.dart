cfb34d1f29957e1b6e13fc8bb55b6dcfafcdf2d8
AI_Nomads <chugue85@gmail.com>
2024-07-11 20:00:28 +0900

    FEAT: link fixed

--- lib/ui/main/search/pages/category_result_page/category_result_page.dart
+++ lib/ui/main/search/pages/category_result_page/category_result_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/_core/constants/line.dart';
 import 'package:untitled/data/model/category_page/category_list.dart';
+import 'package:untitled/data/model/home/book_detail_data.dart';
 import 'package:untitled/ui/common/components/modified_bottom_navigation_bar.dart';
 import 'package:untitled/ui/main/search/pages/category_result_page/_components/back_arrow_appbar.dart';
 import 'package:untitled/ui/main/search/pages/category_result_page/_components/book_grid.dart';
 import 'package:untitled/ui/main/search/pages/category_result_page/_components/result_title.dart';
 import 'package:untitled/ui/main/search/pages/category_result_page/_components/sort_bottm_sheet.dart';
 import 'package:untitled/ui/main/search/pages/category_result_page/_components/sort_section.dart';
-
-import '../../../../../data/model/home/book_detail_data.dart';
-import 'data/category_data.dart';
+import 'package:untitled/ui/main/search/pages/category_result_page/data/category_data.dart';
 
 class CategoryResultPage extends StatefulWidget {
   String? categoryName;
@@ -26,12 +25,20 @@ class _CategoryResultPageState extends State<CategoryResultPage> {
   late String currentCategoryName;
   int _selectedIndex = 0;
   int _curationIndex = 0;
-  String selectedSort = "완독할 확률 높은 순"; // 초기 정렬 기준
+  String selectedSort = "완독할 확률 높은 순";
+  List<Book> books = []; // 초기 상태 설정
+  // 초기 정렬 기준
 
   @override
   void initState() {
     super.initState();
-    currentCategoryName = widget.categoryName!; // 초기 상태 설정
+    currentCategoryName = widget.categoryName!;
+    fetchBooks();
+  }
+
+  void fetchBooks() {
+    books = [bookDetail];
+    setState(() {});
   }
 
   @override

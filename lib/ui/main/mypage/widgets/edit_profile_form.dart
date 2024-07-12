9451d8d8da02698a4b4d5d8e4300f4d9d0a58d75
hillview0303 <ujin0105@gmail.com>
2024-07-12 15:33:28 +0900

    회회원정보 수정

---
+++ lib/ui/main/mypage/widgets/edit_profile_form.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import '../../../../_core/constants/constants.dart';
+
+class EditProfileForm extends StatefulWidget {
+  @override
+  _EditProfileFormState createState() => _EditProfileFormState();
+}
+
+class _EditProfileFormState extends State<EditProfileForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _heightController = TextEditingController();
+  final String _userEmail = "ssar@nate.com";
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    _phoneController.dispose();
+    _heightController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            _userEmail,
+            style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                label: Text('닉네임'),
+                hintText: '닉네임을 입력하세요',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                label: Text('비밀번호'),
+                hintText: '비밀번호를 입력하세요',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+              controller: _confirmPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                label: Text('비밀번호 확인'),
+                hintText: '비밀번호를 다시 입력하세요',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+              controller: _phoneController,
+              decoration: InputDecoration(
+                label: Text('휴대폰번호'),
+                hintText: '휴대폰번호를 입력하세요',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextFormField(
+              controller: _heightController,
+              decoration: InputDecoration(
+                label: Text('주소'),
+                hintText: '주소를 입력하세요',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            child: SizedBox(
+              width: double.infinity,  // Set the button width to full width of the container
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Handle the form submission
+                  }
+                },
+                child: Text('저장'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: kAccentColor2,
+                  foregroundColor: TColor.white,
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+}

4e0bd43a646edcc5fb2c73d4ab14b90fbc56ae8b
AI_Nomads <chugue85@gmail.com>
2024-07-13 11:24:08 +0900

    FIX: navigator focus

--- lib/ui/main/search/search_page.dart
+++ lib/ui/main/search/search_page.dart
@@ -12,7 +12,7 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   String? _selectedOption;
-  final int _selectedIndex = 0;
+  final int _selectedIndex = 1;
 
   @override
   Widget build(BuildContext context) {
